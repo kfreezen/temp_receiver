e3f52f36fc4c2c77aa8786cb40a99e0931a10015
Kent Friesen <kfreezen@gmail.com>
2014-01-30 15:37:38 -0800

    Quick fix to update.sh

--- update.sh
+++ update.sh
@@ -2,9 +2,6 @@
 
 # Extract the tar.gz
 tar -zxf receiver-update.tar.gz
-make
-
-# We should now have an updated receiver.
-systemctl restart receiver # receiver is the name of the receiver service.
+make && systemctl restart receiver # receiver is the name of the receiver service.
 
 # And now latest receiver is running.
