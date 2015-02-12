941e3eb3fd59211a4027a04e59ccec2762336714
Kent Friesen <kfreezen@gmail.com>
2015-02-12 01:48:42 +0000

    Adding a service installer.

---
+++ install-service.sh
@@ -0,0 +1,37 @@
+OS=`lsb_release -si`
+
+INSTALLER=opkg
+
+case $OS in
+	"Angstrom")
+		INSTALLER=opkg
+		UBOOT_UENV=/boot/uEnv.txt
+		REMOVE_LIST="firefox gimp chrome xfce-terminal"
+		;;
+	"Debian")
+		INSTALLER=apt-get
+		UBOOT_UENV=/boot/uboot/uEnv.txt
+		REMOVE_LIST=firefox gimp chromium
+		;;
+	*)
+		echo Error Occurred.
+		exit
+esac
+	
+$INSTALLER remove $REMOVE_LIST
+echo optargs=text > $UBOOT_UENV
+
+cat > /lib/systemd/system/receiver.service <<EOF
+[Unit]
+Description=Start Temp Receiver
+
+[Service]
+Type=simple
+WorkingDirectory=/home/root/receiver
+ExecStart=/home/root/receiver/receiver.sh
+
+[Install]
+WantedBy=default.target
+EOF
+
+systemctl daemon-reload
